15190c492a69deef3fff4cf941847db877381c59
Jacob Schatz <jschatz1@gmail.com>
2019-07-04 17:41:23 -0400

    new examples

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import check from "mcgorgeous"
 
-const sitesSchema = [{ name: "string" }];
-const sitesData = [{ name: "Jeff" }, { name: "Gary" }];
+const sitesSchema = [{ name: "string", id: "number notnull", free: "boolean notnull" }];
+const siteData = [{ name: "Jeff", id: 24601, free: true }, { name: "Gary", id: 22500, free: false }];
 let checkData;
 try{
-	checkData = check(sitesSchema, sitesData);
+	checkData = check(sitesSchema, siteData);
 } catch(e) {
 	checkData = e.message;
 }
-document.body.innerHTML = `<p>Checking data: ${checkData === true ? "good to go!" : checkData}</p>`
+document.body.innerHTML = `
+	<p>Checking data: ${checkData === true ? "good to go!" : checkData}</p>
+	<pre>${JSON.stringify(sitesSchema, null, '\t')}</pre>
+	<pre>${JSON.stringify(siteData, null, '\t')}</pre>
+`
